87ed413cbaa1540e70b5caecde2cdfbbb22ab3d1
유찬규 <dbcksrb99@gmail.com>
2024-02-10 15:05:54 +0900

    feat : 3:25 - 게임씬플로우 진행중

---
+++ Assets/Codes/GameSceneFlow.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameSceneFlow : MonoBehaviour
+{
+    State state;
+
+    public UnityEvent OnReady;
+    public UnityEvent OnPlay;
+    public UnityEvent OnGameOver;
+
+
+    public void ChangeState(State state)
+    {
+        this.state = state;
+        switch
+    }
+
+
+
+
+
+
+    public enum State
+    {
+        Ready, Play, GameOver
+    }
+}
